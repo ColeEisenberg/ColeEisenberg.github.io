63a8a5eb5f2c54d334fa345fe310af9dd9a49ea4
ColeEisenberg <coleridestrek@gmail.com>
2024-09-10 19:48:02 -0400

    Formspree js edit

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
 
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -65,6 +65,6 @@ $(function() {
 
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+/*$('#name').focus(function() {
     $('#success').html('');
 });
